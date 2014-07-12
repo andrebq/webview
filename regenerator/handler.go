28b83b03a9dc9ef42256f48418dc33be9031f74f
Andre Moraes <andre@amoraes.info>
2014-07-12 14:06:52 -0300

    Included a cache to speedup compilation

--- regenerator/handler.go
+++ regenerator/handler.go
@@ -7,12 +7,52 @@
 package regenerator
 
 import (
+	"errors"
 	"github.com/andrebq/gas"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
+
+// CompilerCache links a source file to a regenerated file
+type CompilerCache map[string]string
+
+// Store will link source file to the regenerated one
+func (cc *CompilerCache) Store(source, regen string) {
+	(*cc)[source] = regen
+}
+
+// Open will try to open a regenerated file for the given source file
+// if the mtime from source is after the regenerated, then this will
+// result in a error.
+//
+// If source isn't found, an error is also returned.
+func (cc *CompilerCache) Open(source string) (io.ReadCloser, error) {
+	if regen, has := (*cc)[source]; has {
+		return cc.openIfHot(source, regen)
+	}
+	return nil, errors.New("source not found")
+}
+
+func (cc *CompilerCache) openIfHot(src, regen string) (io.ReadCloser, error) {
+	srcInfo, err := os.Stat(src)
+	if err != nil {
+		return nil, err
+	}
+	regenInfo, err := os.Stat(regen)
+	if err != nil {
+		return nil, err
+	}
+
+	if srcInfo.ModTime().After(regenInfo.ModTime()) {
+		return nil, errors.New("source is newer than regenerated")
+	}
+	return os.OpenFile(regen, os.O_RDONLY, 0644)
+}
 
 // Compiler is responsible for calling npm
 type Compiler struct {
@@ -31,6 +71,11 @@ type Compiler struct {
 
 	// Default parameters
 	RegeneratorParams []string
+
+	// Cache for regenerated files
+	Cache CompilerCache
+
+	tmpDir string
 }
 
 // Compile will read the input file and output the result
@@ -71,6 +116,12 @@ func (c *Compiler) stripPrefix(in string) string {
 }
 
 func (c *Compiler) callRegenerator(input string, out io.Writer) error {
+	reader, err := c.fromCache(input)
+	if err == nil {
+		defer reader.Close()
+		_, err := io.Copy(out, reader)
+		return err
+	}
 	npmCmd := exec.Command(c.Regenerator, append(c.RegeneratorParams, input)...)
 	pipedOut, err := npmCmd.StdoutPipe()
 	if err != nil {
@@ -80,9 +131,55 @@ func (c *Compiler) callRegenerator(input string, out io.Writer) error {
 		return err
 	}
 
-	io.Copy(out, pipedOut)
+	tmpOut, filename, err := c.makeTmpOut(input)
+	if err != nil {
+		// unable to create cached file
+		// write to output
+		io.Copy(out, pipedOut)
+		return npmCmd.Wait()
+	}
+	defer tmpOut.Close()
 
-	return npmCmd.Wait()
+	// we created the temporary output
+	io.Copy(tmpOut, pipedOut)
+	cmdOut := npmCmd.Wait()
+
+	c.ensureCache()
+	c.Cache.Store(input, filename)
+
+	// ensure that everything is on disk
+	tmpOut.Sync()
+	tmpOut.Seek(0, 0)
+	// copy
+	io.Copy(out, tmpOut)
+	return cmdOut
+}
+
+func (c *Compiler) makeTmpOut(input string) (*os.File, string, error) {
+	c.ensureCache()
+	if c.tmpDir == "" {
+		var err error
+		c.tmpDir, err = ioutil.TempDir("", "rengenerator")
+		if err != nil {
+			return nil, "", err
+		}
+	}
+	tmpFile, err := ioutil.TempFile(c.tmpDir, filepath.Base(input))
+	if err != nil {
+		return tmpFile, "", err
+	}
+	return tmpFile, filepath.Join(c.tmpDir, tmpFile.Name()), err
+}
+
+func (c *Compiler) fromCache(input string) (io.ReadCloser, error) {
+	c.ensureCache()
+	return c.Cache.Open(input)
+}
+
+func (c *Compiler) ensureCache() {
+	if c.Cache == nil {
+		c.Cache = make(CompilerCache)
+	}
 }
 
 // Regenerator wrap the compiler under the net/http.Handler interface
